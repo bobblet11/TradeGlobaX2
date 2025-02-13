3e8a333260f3fab0af864031ddbb67b6f6618be2
bobblet11 <benglover78@gmail.com>
2025-02-13 14:34:53 +0800

    fix(CI/CD):

--- test/example.test.js
+++ test/example.test.js
@@ -1,8 +1,8 @@
-import * as chai from 'chai'
+import * as chai from "chai";
 const expect = chai.expect;
 
-describe('Temporary Test Suite', function() {
-  it('should always pass', function() {
-    expect(true).to.be.true;  // This will always pass
+describe("Temporary Test Suite", function () {
+  it("should always pass", function () {
+    expect(true).to.be.true; // This will always pass
   });
 });
